93647690e680cb53b5617c1b2a51e1c37ab89359
Abhik <finnabc123@gmail.com>
2018-07-16 10:35:02 +0530

    Add country's border and population

--- Mapping/map.py
+++ Mapping/map.py
@@ -16,13 +16,23 @@ def colorShow(lt):
         return 'green'
 map = folium.Map(location=[23.011513, 72.516938], zoom_start=5, tiles='OpenStreetMap')
 
-fg = folium.FeatureGroup(name="My Map")
+fgs = folium.FeatureGroup(name="States")
 # add change circle view
 for lt, lo, st in zip(lat,lon, stat):
-    fg.add_child(folium.CircleMarker(location=[lt, lo], popup=st, fill_color=colorShow(lt), radius=7, color='grey', fill_opacity=0.7, fill=True))
+    fgs.add_child(folium.CircleMarker(location=[lt, lo], popup=st, fill_color=colorShow(lt), radius=7, color='grey', fill_opacity=0.7, fill=True))
 # for lt, lo, st in zip(lat,lon, stat):
 #     fg.add_child(folium.CircleMarker(location=[lt, lo], popup=st, icon=folium.Icon(color=colorShow(lt))))
 
-map.add_child(fg)
+# Add Boder of country and Population
+fgp = folium.FeatureGroup(name="Population")
+
+fgp.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig').read(),
+style_function=lambda x:{'fillColor':'green' if x['properties']['POP2005'] < 10000000 
+else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000 else 'red'}))
+
+map.add_child(fgs)
+map.add_child(fgp)
+
+map.add_child(folium.LayerControl())
 
 map.save("mapping.html")
